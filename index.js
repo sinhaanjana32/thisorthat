6dacb486cefcdf371cf728c63c373cc3e32c7776
Anjana <sinhaanjana32@gmail.com>
2020-07-26 23:07:55 +0300

    deploy

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
      app.get('/*', (req, res) => {
       res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
     });
 
-    };
+    }
 
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
