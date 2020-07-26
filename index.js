cb95d856f64345fb106fc077b2a6a577fe0900a8
Anjana <sinhaanjana32@gmail.com>
2020-07-26 22:45:02 +0300

    deploy

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
      app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
 
     };
