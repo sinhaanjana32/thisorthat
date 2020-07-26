01bb8a039fa97af2c68ef0ddfe34af56c3211798
Anjana <sinhaanjana32@gmail.com>
2020-07-26 22:25:27 +0300

    deploy

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/',(req,res) => res.json({hello:'world'}));
+//app.get('/',(req,res) => res.json({hello:'world'}));
 app.use('/api/auth',routes.auth);
 app.use('/api/polls',routes.poll);
 
