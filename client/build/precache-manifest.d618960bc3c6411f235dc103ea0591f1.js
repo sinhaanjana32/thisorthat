4cc0ca821e0d94a4cc4f453618b26889822c508b
Anjana <sinhaanjana32@gmail.com>
2020-07-26 23:18:09 +0300

    deploy

---
+++ client/build/precache-manifest.d618960bc3c6411f235dc103ea0591f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cc7556a2704df1ab89e6bada9589e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b756b8af069e821b5d4",
+    "url": "/static/css/main.907d78d7.chunk.css"
+  },
+  {
+    "revision": "0549c77604d20e2bd85a",
+    "url": "/static/js/2.6b712c8c.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.6b712c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b756b8af069e821b5d4",
+    "url": "/static/js/main.928822fa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
