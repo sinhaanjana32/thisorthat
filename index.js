a4fec646c80a7b1c175101ae99f102e4cbec3676
Anjana <sinhaanjana32@gmail.com>
2020-07-28 01:52:13 +0300

    deploy

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/',(req,res) => res.json({hello:'world'}));
+app.get('/',(req,res) => res.json({hello:'worlddddd'}));
 app.use('/api/auth',routes.auth);
 app.use('/api/polls',routes.poll);
 
@@ -44,7 +44,7 @@ res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
      app.get('/*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 
     }
